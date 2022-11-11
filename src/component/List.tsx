293d9248d2d2e35ecaa670b478c63cf46e9aecc6
Cindy <cdyy2400@gmail.com>
2022-11-11 20:00:20 +0700

    change price color

--- src/component/List.tsx
+++ src/component/List.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Styles from "./List.module.scss";
 
 const List: React.FC<{
@@ -10,13 +10,27 @@ const List: React.FC<{
   changePercent24Hr: string;
   classname: string | undefined;
 }> = (props) => {
+  const [cryptoPrice, setCryptoPrice] = useState(0);
+  const [cryptoPriceColor, setCryptoPriceColor] = useState("black");
+
+  useEffect(() => {
+    setCryptoPrice((prevState) => {
+      prevState > +props.priceUSD
+        ? setCryptoPriceColor("red")
+        : setCryptoPriceColor("green");
+      return +props.priceUSD;
+    });
+  }, [props.priceUSD]);
+
   return (
     <div className={`${Styles.list} ${props.classname}`}>
       <p className={Styles.smallSection}>{props.rank}</p>
       <p className={Styles.smallSection}>{props.name}</p>
       <p className={Styles.mediumSection}>{props.supply}</p>
       <p className={Styles.mediumSection}>{props.marketCapUSD}</p>
-      <p className={Styles.smallSection}>{props.priceUSD}</p>
+      <p className={`${Styles.smallSection} ${Styles[cryptoPriceColor]}`}>
+        {props.priceUSD}
+      </p>
       <p className={Styles.mediumSection}>{props.changePercent24Hr}</p>
     </div>
   );
